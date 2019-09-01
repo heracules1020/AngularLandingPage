7604b1dec9cbc2bcbe4863a0c607f63129b721a5
heracules1020 <heracules1020@outlook.com>
2019-09-01 15:15:40 -0700

    my portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+@Injectable()
 export class AppComponent {
-  title = 'portfoilo';
+  title = 'portfolio';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosectionComponent } from './herosection/herosection.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ContentComponent } from './content/content.component';
+import { TeamComponent } from './team/team.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosectionComponent,
+    HeaderComponent,
+    AboutusComponent,
+    ServiceComponent,
+    TimelineComponent,
+    ContentComponent,
+    TeamComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
